f6fd687e3d01b33580dbd26e645030a116b96683
Murat Halici <murat@murathalici.com>
2012-07-24 00:59:08 -0400

    remove links on comment reply form

--- js/global.js
+++ js/global.js
@@ -5,6 +5,7 @@
       $('.view-home-featured-forum-posts .views-row').addClass('row');
       $('table').addClass('table table-condensed');
       $('.comment-delete a, .comment-edit a, .comment-reply a').addClass('btn');
+      $('.page-comment-reply article.comment ul.links.inline').hide();
 
       // CSS
       $('.node-blog.node-teaser:first').css('padding-top','0px');
