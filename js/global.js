5231036d136d2e596027cab3ac90405c290612d0
Murat Halici <murat@murathalici.com>
2012-07-24 00:48:45 -0400

    add btn class to comment links

--- js/global.js
+++ js/global.js
@@ -3,12 +3,13 @@
     attach: function (context, settings) {
       // Add Classes
       $('.view-home-featured-forum-posts .views-row').addClass('row');
+      $('table').addClass('table table-condensed');
+      $('.comment-delete a, .comment-edit a, .comment-reply a').addClass('btn');
 
       // CSS
       $('.node-blog.node-teaser:first').css('padding-top','0px');
-      $('table').addClass('table table-condensed');
-
       $(".collapse").collapse();
+
     }
   };
 })(jQuery);
