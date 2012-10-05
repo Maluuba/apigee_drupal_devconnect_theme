d68555e91990828d337ce5f3af11893a7f986f63
Murat Halici <murat@murathalici.com>
2012-10-05 14:08:18 -0400

    app performance markup FID-432

--- js/global.js
+++ js/global.js
@@ -17,6 +17,22 @@
       $('.node-blog.node-teaser:first').css('padding-top','0px');
       $(".collapse").collapse();
       $('.page-comment-reply article.comment ul.links.inline').hide();
+
+      $('#reportrange').daterangepicker(
+        {
+          ranges: {
+            'Today': ['today', 'today'],
+            'Yesterday': ['yesterday', 'yesterday'],
+            'Last 7 Days': [Date.today().add({ days: -6 }), 'today'],
+            'Last 30 Days': [Date.today().add({ days: -29 }), 'today'],
+            'This Month': [Date.today().moveToFirstDayOfMonth(), Date.today().moveToLastDayOfMonth()],
+            'Last Month': [Date.today().moveToFirstDayOfMonth().add({ months: -1 }), Date.today().moveToFirstDayOfMonth().add({ days: -1 })]
+          }
+        },
+        function(start, end) {
+          $('#reportrange span').html(start.toString('MMMM d, yyyy') + ' - ' + end.toString('MMMM d, yyyy'));
+        }
+      );
     }
   };
 })(jQuery);
