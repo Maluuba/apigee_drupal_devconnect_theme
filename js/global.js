d4c562c337b57ccc7c9be4d7c188a17be899c34f
Murat Halici <murat@murathalici.com>
2012-10-12 03:46:04 -0400

    stay on tab after form submit

--- js/global.js
+++ js/global.js
@@ -17,6 +17,9 @@
       $('.node-blog.node-teaser:first').css('padding-top','0px');
       $(".collapse").collapse();
       $('.page-comment-reply article.comment ul.links.inline').hide();
+
+      var activeTab = $('[href=' + location.hash + ']');
+      activeTab && activeTab.tab('show');
     }
   };
 })(jQuery);
